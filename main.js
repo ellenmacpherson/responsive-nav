1e98eb90fabc11fd48cda0c3c812c7a32ebb07cc
Ellen Macpherson <macphersonellen@outlook.com>
2019-04-02 13:14:41 +0100

    adding hamburger menu for smaller devices

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  $(".topnav").hide();
+  $("#cross").hide();
 
-  $(".menu").click(function(){
-    $(".topnav").slideToggle(250);
-});
+    $('.mobile-menu').hide();
+    $('.desktop-menu').show();
+
+    $('#menu-text').click(function() {
+      $(".topnav").slideToggle("slow");
+      $("#cross").show();
+      $("#menu-text").hide();
+    })
+
+    $("#cross").click(function() {
+      $(".topnav").slideToggle("slow");
+      $("#menu-text").show();
+      $("#cross").hide();
+    });
+
+  if ($(window).width() <= 800) {
+    $('.mobile-menu').show();
+    $('.desktop-menu').hide();
+
+    $('#hamburger').click(function() {
+      $(".topnav").slideToggle("slow");
+    });
+  }
 
 });
